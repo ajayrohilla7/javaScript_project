28562d6db75a46e8c1a0e9d0d21b5721d31cc141
AJAY ROHILLA <65978835+ajayrohilla7@users.noreply.github.com>
2025-01-29 16:24:33 +0000

    oprtrs

--- .devcontainer/test/test01.js
+++ .devcontainer/test/test01.js
@@ -1,7 +1,12 @@
 console.log("Ajay")
-let Intname ="Rohilla"
-let dob =7
-let city ="indore"
+// let Intname ="Rohilla"
+// let dob =7
+// let city ="indore"
 
-console.log("Dob is of type "+typeof(dob))
-console.table([Intname,dob,city])
+
+// console.log("Dob is of type "+typeof(dob))
+// console.table([Intname,dob,city])
+ let x=10
+ y=x++
+ console.log(x,"",y)
+ console.log(y)
